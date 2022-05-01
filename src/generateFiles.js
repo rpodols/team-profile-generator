574cee25fd6ec48bfefe5e047b5d3ceb4a4f747c
ryanp <rdpodols@gmail.com>
2022-05-01 10:29:24 -0400

    working application

--- src/generateFiles.js
+++ src/generateFiles.js
@@ -1,21 +1,20 @@
 function generateHtml(data) {
-
     let newTeamArray = data.map(function(data){
-        differentLine = `<p class="card-text">Office Number: ${data.officeNumber}</p>`;
+        differentLine = `<p class="card-text card-styling">Office Number: ${data.officeNumber}</p>`;
         if (data.role === 'Engineer'){
-            differentLine = `<p class="card-text">GitHub: <a href='https://github.com/${data.github}' target="_blank">${data.github}</a></p>`;
+            differentLine = `<p class="card-text card-styling">GitHub: <a href='https://github.com/${data.github}' target="_blank">${data.github}</a></p>`;
         };
         if (data.role === 'Intern'){
-            differentLine = `<p class="card-text">School: ${data.school}</p>`;
+            differentLine = `<p class="card-text card-styling">School: ${data.school}</p>`;
         }
         return `<div class="card" style="width: 18rem;">      
             <div class="card-body">
-                <div class="p-3 mb-2 bg-primary text-white text-center">
+                <div class="p-3 mb-2 bg-primary text-white text-center card-padding">
                     <h5 class="card-title"><i class="${data.image}"></i>   ${data.name}</h5>
                     <p class="card-text">Role: ${data.role}</p>
                 </div>
-                <p class="card-text">ID: ${data.id}</p>
-                <p class="card-text">Email: <a href=mailto:${data.email}>${data.email}</a></p>
+                <p class="card-text card-styling">ID: ${data.id}</p>
+                <p class="card-text card-styling">Email: <a href=mailto:${data.email}>${data.email}</a></p>
                 ${differentLine}
             </div>
         </div>`
@@ -30,11 +29,12 @@ var joinedArray = newTeamArray.join(" ");
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Document</title>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="../assets/css/style.css">
         <script src="https://kit.fontawesome.com/c6a7527302.js" crossorigin="anonymous"></script>
     </head>
 
     <body>
-        <div class="p-3 mb-2 bg-danger text-white text-center">My Team</div>
+        <div class="mb-2 bg-danger text-white text-center header-design">My Team</div>
         <div class="container">
         <div class="row justify-content-around">
             ${joinedArray}
@@ -46,7 +46,7 @@ var joinedArray = newTeamArray.join(" ");
     </html>`
 
         
-    return html;
+    return html; 
 };
 
 module.exports = generateHtml;
